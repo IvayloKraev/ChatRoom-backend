d6e04c91c27cb978e02ba1943a290a0e28a9e30c
IvayloKraev <ivaylokraev@protonmail.com>
2024-07-22 12:32:45 +0300

    Add header functions for receiving and sending messages

--- inc/wsServer.hpp
+++ inc/wsServer.hpp
@@ -1,6 +1,14 @@
 #ifndef WSSOCKET_HPP
 #define WSSOCKET_HPP
 
+#include <memory>
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+#include "ixwebsocket/IXWebSocketMessage.h"
+#include "ixwebsocket/IXWebSocket.h"
+#include "ixwebsocket/IXConnectionState.h"
 #include "ixwebsocket/IXWebSocketServer.h"
 
 namespace wsServer
@@ -10,6 +18,11 @@ namespace wsServer
     namespace _inner
     {
         extern std::unique_ptr<ix::WebSocketServer> srv;
+
+        extern std::thread sendThread;
+
+        void routeClientMessage(const std::shared_ptr<ix::ConnectionState>&, ix::WebSocket&, const ix::WebSocketMessagePtr&);
+        void sendMessage();
     }
 }
 
