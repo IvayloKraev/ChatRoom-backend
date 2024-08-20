67befbe6d28f47aac50ea6a5bd3087b2f6d8220f
IvayloKraev <ivaylokraev@protonmail.com>
2024-08-20 09:55:45 +0300

    Create test

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,36 @@
 #include <thread>
 #include <chrono>
 
-#include <wsServer.hpp>
+//#include <wsServer.hpp>
+#include "dataWraper.hpp"
 
-[[noreturn]] int main()
+int main()
 {
-    wsServer::init();
+//    wsServer::init();
     std::cout << "Hello, World!" << std::endl;
+
+    DataWraper<int> q;
+
+    std::thread first = std::thread([&q](){
+        while (true){
+            q.push(7);
+            std::this_thread::sleep_for(std::chrono::milliseconds(700));
+        }
+    });
+
+    std::thread second = std::thread([&q](){
+        while (true){
+            q.push(19);
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+    });
+
+    std::thread third = std::thread([&q](){
+        while (true){
+            auto temp = q.pop();
+            std::cout << temp << std::endl;
+        }
+    });
 
     while (true)
     {
