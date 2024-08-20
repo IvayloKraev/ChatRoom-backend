1ab446aa875fa93e7787f449253e2e715937f852
IvayloKraev <ivaylokraev@protonmail.com>
2024-08-20 09:55:32 +0300

    Refactor the thread conditions

--- inc/dataWraper.hpp
+++ inc/dataWraper.hpp
@@ -18,29 +18,27 @@ public:
 
         _dataQueue.push(std::move(value));
 
-        _inStream.notify_one();
+        _waitForPush.notify_one();
     }
 
     valueT pop()
     {
         std::unique_lock<std::mutex> lock_guard(_controlerMutex);
 
-        _inStream.wait(lock_guard, [&]()
+        _waitForPush.wait(lock_guard, [&]()
         {
             return !_dataQueue.empty();
         });
 
         valueT valueForPop = std::move(_dataQueue.front());
         _dataQueue.pop();
-        _outStream.notify_all();
         return std::move(valueForPop);
     }
 
 private:
     std::queue<valueT> _dataQueue;
     std::mutex _controlerMutex;
-    std::condition_variable _inStream;
-    std::condition_variable _outStream;
+    std::condition_variable _waitForPush;
 };
 
 #endif
